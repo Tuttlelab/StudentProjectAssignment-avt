28c73732f81d23c74cc0207d2f88f391a46d565e
avanteijlingen <57176642+avanteijlingen@users.noreply.github.com>
2023-05-31 23:02:14 +0100

    updated the readme page

--- Hungarian.py
+++ Hungarian.py
@@ -7,34 +7,67 @@ Created on Wed May 31 19:22:30 2023
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas
+import pandas, sys, copy
 from scipy.optimize import linear_sum_assignment
 #https://docs.scipy.org/doc/scipy-0.18.1/reference/generated/scipy.optimize.linear_sum_assignment.html
 #The student that didnt make 10 choices gets his first choice as 2 instead of 1 for doing it wrong
 
 pandas.set_option('display.max_columns', 10)
 
-ChoiceMin = 1
-ChoiceMax = 88
 
-projects = choices = pandas.read_csv("Project List by Section.csv", index_col=0)
+projects = pandas.read_csv("Project List by Section.csv", index_col=0)
+assert (projects["Capacity"].values < 10).all()
+
+ChoiceMin = min(projects.index)
+ChoiceMax = max(projects.index)
+
+# We allow for projects with capacity > 1 by making project 87 into projects 87 & 87.1
+for i in projects.index:
+    if projects.at[i, "Capacity"] > 1:
+        for addition in range(1, projects.at[i, "Capacity"].astype(np.int64) +1):
+            projects.at[i+(addition/10), "Capacity"] = 1    
+            projects.at[i+(addition/10), "Section"] = projects.at[i, "Section"]    
+        projects.at[i, "Capacity"] = 1
+
+projects = projects.sort_index()
+print(projects.iloc[-5:])
+
+
+
 choices = pandas.read_csv("Project-Data.csv", index_col=0)
 choices.columns = [int(x) for x in choices.columns]
 Result = pandas.DataFrame(index=choices.index, columns=["Project"])
 print(choices)
 
-CostMatrix = pandas.DataFrame(index=choices.index, columns=np.arange(1,88))
+#Adjust the choices to account for projects with a capacity > 1
+DecimalChoices = copy.copy(choices)
+for student in choices.index:
+    new_choice_list = []
+    for choice in choices.loc[student].values:
+        new_choice_list.append(choice)
+        for decimal in np.arange(0.1, 1.0, 0.1):
+            if float(choice)+decimal in projects.index:
+                new_choice_list.append(float(choice)+decimal)
+            else:
+                break
+    print(new_choice_list)
+    for i in range(10):
+        DecimalChoices.at[student, i+1] = new_choice_list[i]
+print(DecimalChoices)
 
+
+CostMatrix = pandas.DataFrame(index=DecimalChoices.index, columns=projects.index)
 #its a cost matrix so we want to price the choices low and the projects not chosen very highly
-HighCost = 100
+HighCost = 10000
 CostMatrix[:] = HighCost
 
-for student in choices.index:
-    #Cost penality for invalid choices
-    CostPenalty = np.where((choices.loc[student] < ChoiceMin) | (choices.loc[student] > ChoiceMax))[0].shape[0]
-    ValidChoices = np.where((choices.loc[student] > ChoiceMin) & (choices.loc[student] < ChoiceMax))[0]
-    for cost,choice in enumerate(choices.loc[student].iloc[ValidChoices]):
-        CostMatrix.at[student, choice] = cost + CostPenalty
+for student in DecimalChoices.index:
+    #Cost penality for invalid DecimalChoices
+    CostPenalty = np.where((DecimalChoices.loc[student] < ChoiceMin) | (DecimalChoices.loc[student] > ChoiceMax))[0].shape[0]
+    ValidChoices = np.where((DecimalChoices.loc[student] > ChoiceMin) & (DecimalChoices.loc[student] < ChoiceMax))[0]
+    for cost,choice in enumerate(DecimalChoices.loc[student].iloc[ValidChoices]):
+        CostMatrix.at[student, choice] = (cost**2) + CostPenalty
+        #CostMatrix.at[student, choice] = cost + CostPenalty
 print(CostMatrix)
 
 
@@ -54,6 +87,7 @@ Result = pandas.DataFrame(index=choices.index, columns=["Project", "Choice #"])
 for i in range(len(row_ind)):
     student = CostMatrix.index[row_ind[i]]
     project = CostMatrix.columns[col_ind[i]]
+    project = np.floor(project).astype(np.int64)
     Result.at[student, "Project"] = project
     try:
         Result.at[student, "Choice #"] = np.where(choices.loc[student] == project)[0][0]+1
@@ -65,6 +99,10 @@ print(Result)
 
 print("="*50)
 print("Result summary:")
+print("Number of students that got their first choice:", (Result["Choice #"] == 1).sum())
+print("Number of students that got their second choice:", (Result["Choice #"] == 2).sum())
+print("Number of students that got their third choice:", (Result["Choice #"] == 3).sum())
+
 print("Average choice that each student got:", Result[Result["Choice #"] != -1]["Choice #"].values.astype(np.float64).mean())
 print("Did any student get assigned a project that wasnt on their list of choices:", (Result["Choice #"] == -1).any())
 
--- README.md
+++ README.md
@@ -1,45 +1,27 @@
 # StudentProjectAssignment-avt
 
-There are a number of algorithms that can be used to assign students to projects. Some of the most common algorithms include:
 
 
 	
-##### The Hungarian algorithm 
-is a linear-time algorithm that can be used to find a stable matching between students and projects. A stable matching is one in which no student and project prefer each other to their current assignment.
-	
-### The auction algorithm 
-is a heuristic algorithm that can be used to find a good solution to the student-project assignment problem. It is not guaranteed to find the optimal solution, but it is often very efficient.
-	
-### The greedy algorithm
-is a simple algorithm that can be used to find a good solution to the student-project assignment problem. It is not guaranteed to find the optimal solution, but it is often very efficient.
+### The Hungarian algorithm 
+*Hungarian.py* takes the list of projects and their capacities in "Project List by Section.csv" and the students list of choices by "Project-Data.csv" and spits out "Project-assignment.csv"
 
+We allow for projects with capacity > 1 by making project 87 into projects 87 & 87.1
 
-## THE CHALLENGE
+### Penalties
+If students have made invalid choices they have the cost penality incremented for each of their other choices for each invalid choice they made, this includes not making 10 choices. For instance if a student makes 9 choices instead of 10 then their first choice will be their second choice, and there will be no first choice. This doesnt mean they can't still get their first choice just then they will be penalized against other students who have the same first choice.
 
-Write a script that will assign students to projects. The data is provided in the excel spreadsheet (attached). You want to optimise for all students receiving the highest priority project possible. I have the distribution of those receiving their first choice, second choice, etc. from the random assignment process, so that is your benchmark.
-
-##CONSTRAINTS
-
-The spreadsheet also contain a page with constraints on the number of students to be allocated to each project, not all are one, so be careful.
-
-The spreadsheet also has which Section the project would be supervised in, this is something that should be monitored but it is not (at this point) an optimisation criteria.
-
-
-
-##THE WINNER
-
-The winner will be decided by who creates a program that generates the highest level of 1st, 2nd, 3rd, etc. allocations.
-
-In the event of a tie, bonus points will be awarded for bells and whistles, such as:
-
+### Limitations:
+- Assumes there is no project with capacity > 9
 
 	
-A self contained package that can read in data exported from the excel file and return data and statistics that are readable as an excel file (e.g., csv data) - or do it in VBA if you want!
-	
-If you can think of any exciting, humorous or useful additions to the ranking that are also implemented as required...
 
-## THE PRIZE
-FREE DRINKS following the SCoTCH Research Day and potentially acknowledgment and recognition from generations of students if you're script is taken forward and implemented as Departmental policy.
- 
-## DEADLINE: 
-Submit entries (program and output) to TT 1 week before the SCoTCH research day so I can test and verify results. The winner will be announced on the day.... Happy coding 
+#### Results
+~~~~
+Result summary:
+Number of students that got their first choice: 43
+Number of students that got their second choice: 20
+Number of students that got their third choice: 17
+Average choice that each student got: 1.8470588235294119
+~~~~
+
